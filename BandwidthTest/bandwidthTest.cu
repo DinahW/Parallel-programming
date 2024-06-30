69f3bbd58159b8887d3d3187e7f92bfe402a8f81
DinahW <56730690+DinahW@users.noreply.github.com>
2024-06-30 22:55:14 +0200

    Update bandwidthTest.cu

--- BandwidthTest/bandwidthTest.cu
+++ BandwidthTest/bandwidthTest.cu
@@ -1,21 +1,7 @@
 
-/*
- * Copyright 1993-2015 NVIDIA Corporation.  All rights reserved.
- *
- * Please refer to the NVIDIA end user license agreement (EULA) associated
- * with this source code for terms and conditions that govern your use of
- * this software. Any use, reproduction, disclosure, or distribution of
- * this software and related documentation outside the terms of the EULA
- * is strictly prohibited.
- *
- */
 
-/*
- * This is a simple test program to measure the memcopy bandwidth of the GPU.
- * It can measure device to device copy bandwidth, host to device copy bandwidth
- * for pageable and pinned memory, and device to host copy bandwidth for pageable
- * and pinned memory.
- *
+
+ /*
  * Usage:
  * ./bandwidthTest [option]...
  */

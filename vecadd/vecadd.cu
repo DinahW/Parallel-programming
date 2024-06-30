b4242d9873d8616488c8a5aecfc20b775c2f999d
DinahW <56730690+DinahW@users.noreply.github.com>
2024-06-30 22:00:02 +0200

    Update vecadd.cu

--- vecadd/vecadd.cu
+++ vecadd/vecadd.cu
@@ -17,7 +17,7 @@ __global__ void vector_add(const float *a,
 {
   // compute the global element index this thread should process
   /* insert code to calculate the index properly using blockIdx.x, blockDim.x, threadIdx.x */
-  unsigned int index =blockDim.x * blockIdx.x + threadIdx.x + 2; /*FIXME/Fixed #4*/
+  unsigned int index =blockDim.x * blockIdx.x + threadIdx.x; /*FIXME/Fixed #4*/
 
   // avoid accessing out of bounds elements
   if(index < n)
@@ -51,9 +51,10 @@ int main(void)
 
 
   // cudaMalloc the device arrays
-  cudaMalloc((void**)&d_a, size); /*FIXME #1*/);//FIXED
-  cudaMalloc((void**)&d_b, size); /*FIXME #1*/);//FIXED
-  cudaMalloc((void**)&d_c, size); /*FIXME #1*/);//FIXED
+  cudaMalloc((void **)&device_array_a, size); /*FIXME #1/FIXED */
+  cudaMalloc((void **)&device_array_b, size); /*FIXME #1/FIXED */
+  cudaMalloc((void **)&device_array_c, size); /*FIXME #1/FIXED*/
+
 
   // if any memory allocation failed, report an error message
   if(host_array_a == 0 || host_array_b == 0 || host_array_c == 0 ||
@@ -75,8 +76,8 @@ int main(void)
 
   // copy arrays a & b to the device memory space
   /* fix the parameters needed to copy data to the device */
-  cudaMemcpy(d_a, &a, size, cudaMemcpyHostToDevice); /* FIXME #2*/ );//FIXED
-  cudaMemcpy(d_b, &b, size, cudaMemcpyHostToDevice); /* FIXME #2*/ );//FIXED
+  cudaMemcpy(device_array_a, size, cudaMemcpyHostToDevice); /*FIXME #2/Fixed */
+  cudaMemcpy(device_array_b, size, cudaMemcpyHostToDevice); /*FIXME #2/Fixed */
 
   // compute c = a + b on the device
   const size_t nThreads = 256;
@@ -86,10 +87,10 @@ int main(void)
   if(num_elements % nThreads) ++nBlocks;
 
   // launch the kernel
-  vector_add<<<1,N >>>(d_a, d_b, d_c,N);//FIXED
+  vector_add<<<1,N >>>(device_array_a, device_array_b, device_array_c,num_elements);//FIXED
 
   // copy the result back to the host memory space
-  cudaMemcpy(&c, d_c, size, cudaMemcpyDeviceToHost);//FIXED
+  cudaMemcpy(&c, device_array_c, size, cudaMemcpyDeviceToHost);//FIXED
 
   // print out the first 10 results
   for(int i = 0; i < 10; ++i)
@@ -102,9 +103,9 @@ int main(void)
   free(host_array_b);
   free(host_array_c);
 
-  cudaFree(d_a/*FIXME6*/);//FIXED
-  cudaFree(d_b/*FIXME6*/);//FIXED
-  cudaFree(d_c/*FIXME6*/);//FIXED
+  cudaFree(device_array_a/*FIXME6*/);//FIXED
+  cudaFree(device_array_b/*FIXME6*/);//FIXED
+  cudaFree(device_array_c/*FIXME6*/);//FIXED
 
   return 0;
 }
